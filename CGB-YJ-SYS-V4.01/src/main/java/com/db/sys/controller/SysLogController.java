d2d4b3a48a0d7d00798d459248113fe32ad2f70e
wozhendemeiyoumingzi <38720033+wozhendemeiyoumingzi@users.noreply.github.com>
2019-02-21 19:56:17 +0800

    项目
    项目峰会

---
+++ CGB-YJ-SYS-V4.01/src/main/java/com/db/sys/controller/SysLogController.java
@@ -0,0 +1,13 @@
+package com.db.sys.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/log/")
+public class SysLogController {
+	@RequestMapping("doLogListUI")
+	public String doLogListUI() {
+		return"sys/log_list";
+	}
+}
